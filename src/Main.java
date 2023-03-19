02ecd53874aa510117e321313c52ec9f2f3c9829
Alexandros Mavromatis <alexandmi@csd.auth.gr>
2023-03-19 21:42:36 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import general.GameController;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException,ClassNotFoundException {
+
+        GameController game = new GameController();
+    }
+}
